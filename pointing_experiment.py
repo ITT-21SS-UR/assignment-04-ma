4bd7fc5bfa2b836410a3655996ba1e48ff469409
A. Alesik <33415083+AAlesik@users.noreply.github.com>
2021-05-10 22:05:52 +0200

    Test working -- setup ini remains

--- pointing_experiment.py
+++ pointing_experiment.py
@@ -25,15 +25,12 @@ ID = 345
 pd.set_option("display.max_rows", None, "display.max_columns", None)
 
 
-
-
-
 class Test:
     def __init__(self):
         # self.color_palette = pd.read_csv(url_color_csv)
         self.column_names = ["ID", "Condition", "Repetition", "Targetindex",
                              "Targetposition(absolute)", "Targetsize(absolute)",
-                             "Timestamp(Teststart)", "Timestamp(Rep_load)"]
+                             "Timestamp(Teststart)", "Timestamp(Rep_load)", "Timestamp(clicked)"]
         self.log_data = pd.DataFrame(columns=self.column_names)
         self.path_results = "result.csv"
         self.participant_ID = 345
@@ -79,18 +76,16 @@ class Test:
     # getter and setter:
 
     def set_timestamp(self, rep_status, case):
-        if case == 0:
-            self.log_data[self.column_names[4]] = time.time()
+        if case == 2:
+            self.log_data[self.column_names[4 + case]] = time.time()
             return
-        self.log_data.loc[rep_status, self.column_names[case + 8]] = time.time()
+        self.log_data.loc[rep_status, self.column_names[case + 4]] = time.time()
 
     def set_target_abs_pos(self, rep_status, pos_x, pos_y):
         self.log_data.loc[rep_status, self.column_names[4]] = str(pos_x) + ", " + str(pos_y)
 
     def get_current_target(self, rep_status):
         return self.log_data.loc[rep_status, self.column_names[3]]
-
-
 
 
 class PointingExperiment(QDialog):
@@ -101,37 +96,40 @@ class PointingExperiment(QDialog):
         self.canvas_margin_top = 70
         self.canvas_margin_default = 15
         self.test_started = False
-        self.test = Test()
-        self.test.create_test(str(ID))
         self.current_target_pos_x = 0
         self.current_target_pos_y = 0
         self.current_target_index = 0
         self.current_repetition = 0
+        self.test = Test()
+        self.test.create_test(str(ID))
         self.initUI()
-
 
     def start_test(self):
         self.test_started = True
+        self.test.set_timestamp(self.current_repetition, 2)
         self.start_Button.hide()
         return
 
     def paintEvent(self, event):
-        self.label.setText("ID: " + str(ID) + " - " + str(self.current_repetition) + "/" + str(REPETITIONS))
+        self.label.setText("ID: " + str(ID) + " - " + str(self.current_repetition + 1) + "/" + str(REPETITIONS))
         if self.test_started:
+
             painter = QPainter(self)
             painter.setPen(QPen(Qt.green, 2, Qt.SolidLine))
             index = 0
             for i in range(0, TARGETS_PER_ROW):
                 for j in range(0, TARGETS_PER_COLUMN):
                     painter.setBrush(QBrush(Qt.transparent, Qt.SolidPattern))
-                    pos_x = random.randrange(0, TARGETSPACE - TARGETSIZE) + i*TARGETSPACE + self.canvas_margin_default
-                    pos_y = random.randrange(0, TARGETSPACE - TARGETSIZE) + j*TARGETSPACE + self.canvas_margin_top
+                    pos_x = random.randrange(0, TARGETSPACE - TARGETSIZE) + i * TARGETSPACE + self.canvas_margin_default
+                    pos_y = random.randrange(0, TARGETSPACE - TARGETSIZE) + j * TARGETSPACE + self.canvas_margin_top
                     if index == self.test.get_current_target(self.current_repetition):
                         self.current_target_pos_x = pos_x
                         self.current_target_pos_y = pos_y
-                        painter.setBrush(QBrush(Qt.red, Qt.SolidPattern))
+                        painter.setBrush(QBrush(Qt.darkGreen, Qt.SolidPattern))
+                        self.test.set_target_abs_pos(self.current_repetition, pos_x, pos_y)
                     painter.drawEllipse(pos_x, pos_y, TARGETSIZE, TARGETSIZE)
                     index = index + 1
+            self.test.set_timestamp(self.current_repetition, 3)
 
     def check_input(self, m_pos_x, m_pos_y):
         if self.current_target_pos_x > m_pos_x:
@@ -143,12 +141,27 @@ class PointingExperiment(QDialog):
         if self.current_target_pos_x + TARGETSIZE < m_pos_x:
             return
         print("success!!")
+        self.test.set_timestamp(self.current_repetition, 4)
+        print(self.test.log_data)
+        self.update()
+
+    def update(self):
+        super().update()
+        if self.test_started:
+            if self.current_repetition + 1 < REPETITIONS:
+                self.current_repetition = self.current_repetition + 1
+                return
+            self.test_started = False
+            self.test.save_test()
+            self.close()
+
+
+
 
     def mousePressEvent(self, event):
-        if event.button() == QtCore.Qt.LeftButton:
-            self.check_input(event.x(), event.y())
-
-
+        if self.test_started:
+            if event.button() == QtCore.Qt.LeftButton:
+                self.check_input(event.x(), event.y())
 
     def initUI(self):
         # initialize important ui-components
@@ -160,13 +173,11 @@ class PointingExperiment(QDialog):
             width = CANVAS_MIN_WIDTH
         if height < CANVAS_MIN_HEIGHT:
             height = CANVAS_MIN_HEIGHT
-        self.setFixedSize(width+2*self.canvas_margin_default, height+2*self.canvas_margin_default+self.canvas_margin_top)
+        self.setFixedSize(width + 2 * self.canvas_margin_default,
+                          height + 2 * self.canvas_margin_default + self.canvas_margin_top)
         self.label.setText("ID: " + str(ID) + " - 0/" + str(REPETITIONS))
         self.start_Button.clicked.connect(lambda: self.start_test())
 
-    def currentTarger(self):
-
-        return
 
 
 def main():
